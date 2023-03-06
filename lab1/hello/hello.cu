68c1057866bd2d8ebc3c2eff1e973bc6b314be9d
Giorgio Sepe <giorgio.sepe@studenti.unimi.it>
2023-03-06 11:40:35 +0100

    various tests

--- lab1/hello/hello.cu
+++ lab1/hello/hello.cu
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <iostream>
+#include <unistd.h>
+
 
 using namespace std;
 
 __global__ void helloFromGPU (void) {
-    int tID = threadIdx.x;
-    printf("Hello World from GPU (I'am thread = %d)!\n", tID);
+  int tID = threadIdx.x;
+  printf("Hello World from GPU (I'am thread %d)!\n", tID);
 }
 
 int main(void) {
-    // hello from GPU 
-    cout << "Hello World from CPU!" << endl;
-    cudaSetDevice(1);
-    helloFromGPU <<<1, 10>>>();
-    cudaDeviceSynchronize();
-    return 0;
+  //# hello from GPU 
+  cout << "Hello World from CPU!" << endl;
+  cudaSetDevice(0);
+  helloFromGPU <<<1, 10>>>();
+  cudaDeviceSynchronize();
+  sleep(10);
+  return 0;
 }
