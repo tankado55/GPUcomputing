5c3cd90782d6fb2ece5ba8ca6e88c4a24659950c
Giorgio <mog955@gmail.com>
2023-04-03 13:43:11 +0200

    minor

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jio/projects/GPUcomputing/lab1/MQDB
-# Build directory: /home/jio/projects/GPUcomputing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
